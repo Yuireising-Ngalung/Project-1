d5c3a477ccf4a24ef34df2c2cafce3edd7da41d1
Yuireising <yuireisingngalung2000@gmail.com>
2024-10-14 12:39:29 +0530

    hero-section

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  const showMenu = document.querySelector(".sidemenu");
+  showMenu.style.display = "block";
+}
+
+function closeMenu() {
+  const showMenu = document.querySelector(".sidemenu");
+  showMenu.style.display = "none";
+}
